7522799bb93b3380e43a57da376750ce5c81ac86
Vi Tran <Vi.n.tran.23@dartmouth.edu>
2021-04-15 16:21:42 -0700

    set up

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log('starting up!');
+import $ from 'jquery';
+import './style.scss';
+
+let counter = 0;
+setInterval(() => { counter += 1; $('#main').html(`You've been on this page for ${counter} seconds.`); }, 1000);
+
+console.log('hfjadf');
