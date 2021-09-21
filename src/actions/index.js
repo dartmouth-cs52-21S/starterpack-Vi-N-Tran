faee0d83f4b333f0009b05682a00d12ba14bd733
Vi Tran <Vi.n.tran.23@dartmouth.edu>
2021-09-21 00:23:54 -0400

    add thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -17,3 +17,52 @@ export function decrement() {
     payload: null,
   };
 }
+
+// example calls
+
+/* GET */
+// export function fetchPosts() {
+//   return (dispatch) => {
+//     axios.get(`${ROOT_URL}/posts`).then((response) => {
+//       dispatch({
+//         type: ActionTypes.FETCH_POSTS,
+//         payload: response.data,
+//       });
+//     }).catch((error) => {
+//       console.log(`Error in fetchPosts: ${error}`);
+//       dispatch({ type: ActionTypes.ERROR, payload: { message: 'There is an error while fetching all posts', data: error } });
+//     });
+//   };
+// }
+
+/* PUT */
+// export function updatePost(id, newPost) {
+//   return (dispatch) => {
+//     axios.put(`${ROOT_URL}/posts/${id}`, newP, { headers: { authorization: localStorage.getItem('token') } }).then((response) => {
+//       dispatch({
+//         type: ActionTypes.UPDATE_POST,
+//         payload: response.data,
+//       });
+//     }).catch((error) => {
+//       console.log(`Error in updatePost: ${error}`);
+//       dispatch({ type: ActionTypes.ERROR, payload: { message: 'There is an error while updating one post', data: error } });
+//     });
+//   };
+// }
+
+/* POST */
+// export function createPost(newPost, history) {
+//   return (dispatch) => {
+//     axios.post(`${ROOT_URL}/posts`, newP, { headers: { authorization: localStorage.getItem('token') } }).then((response) => {
+//       dispatch({
+//         type: ActionTypes.CREATE_POST,
+//         payload: response.data,
+//       });
+//       history.push('/');
+//     }).catch((error) => {
+//       console.log(`Error in createPost: ${error}`);
+//       dispatch({ type: ActionTypes.ERROR, payload: { message: 'There is an error while creating post', data: error } });
+//       history.push('/');
+//     });
+//   };
+// }
---
+++ src/reducers/example-reducer.js
@@ -0,0 +1,25 @@
+// import { ActionTypes } from '../actions';
+
+// const initialState = {
+//   all: [],
+//   current: {},
+// };
+
+// const PostsReducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case ActionTypes.FETCH_POSTS:
+//       return { ...state, all: action.payload, current: action.payload[0] };
+//     case ActionTypes.FETCH_POST:
+//       return { ...state, current: action.payload };
+//     case ActionTypes.UPDATE_POST:
+//       return { ...state, current: action.payload };
+//     case ActionTypes.CREATE_POST:
+//       return { ...state, current: action.payload };
+//     case ActionTypes.DELETE_POST:
+//       return { ...state };
+//     default:
+//       return state;
+//   }
+// };
+
+// export default PostsReducer;
